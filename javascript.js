509bbbe8762d48097d7d0521cf38b7d8bfa77ca3
robertoschlott <163012909+robertoschlott@users.noreply.github.com>
2024-09-16 21:54:22 -0300

    salvar

---
+++ javascript.js
@@ -0,0 +1,37 @@
+<script>
+    function tocaSomPom () {
+        document.querySelector('#som_tecla_pom').play()
+    }
+
+    function tocaSomClap () {
+        document.querySelector('#som_tecla_clap').play()
+    }
+
+    function tocaSomTim () {
+        document.querySelector('#som_tecla_tim').play()
+    }
+
+    function tocaSomPuff () {
+        document.querySelector('#som_tecla_puff').play()
+    }
+
+    function tocaSomSplash () {
+        document.querySelector('#som_tecla_splash').play()
+    }
+
+    function tocaSomToim () {
+        document.querySelector('#som_tecla_toim').play()
+    }
+
+    function tocaSomPsh () {
+        document.querySelector('#som_tecla_psh').play()
+    }
+
+    function tocaSomTic () {
+        document.querySelector('#som_tecla_tic').play()
+    }
+
+    function tocaSomTom () {
+        document.querySelector('#som_tecla_tom').play()
+    }
+</script>
